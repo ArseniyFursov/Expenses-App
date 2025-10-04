c8b7d95115f72074f3d14c4976f92d89fe18a690
ArseniyFursov <akseniy.life@gmail.com>
2025-10-04 21:57:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,23 +5,27 @@ const btnNode = document.querySelector('.js-btn');
 const totalNode = document.querySelector('.js-total');
 const stateNode = document.querySelector('.js-state');
 const historyNode = document.querySelector('.js-history');
+const limmitNode = document.querySelector('.limit');
 
 btnNode.addEventListener('click', function() {
-    if (!inputNode) {
+    if (!inputNode.value) {
         return
     }
-
     const expense = parseInt(inputNode.value);
-
     expenses.push(expense);
-
     inputNode.value = '';
-
     let sum = 0;
-
+    let expensesListHTML='';
     expenses.forEach(element => {
-        sum += element
+        sum += element;
 
-
+        expensesListHTML += `<li>${element}</li>`;
     });
+    historyNode.innerHTML=`<ol>${expensesListHTML}</ol>`;
+    totalNode.innerHTML= `<p>${sum}</p>`;
+    if (sum <= parseInt(limmitNode.innerHTML)) {
+        stateNode.innerHTML = `<p style="color:rgba(3, 145, 0, 1);">Все хорошо</p>`;
+    } else {
+        stateNode.innerHTML = `<p style="color:rgba(252, 0, 0, 1);">Все плохо</p>`;
+    }
 });
